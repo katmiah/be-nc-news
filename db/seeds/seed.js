df340c0e7354043a4e98c02dbb6bf1f106b35350
Katie Hardy <katiehardy@macbookpro.home>
2025-03-05 16:27:35 +0000

    articleid null

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,18 +2,18 @@ const db = require("../connection")
 const format = require("pg-format");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query("DROP TABLE IF EXISTS comments")
+  return db.query("DROP TABLE IF EXISTS comments CASCADE")
   .then(()=>{
-    return db.query("DROP TABLE IF EXISTS articles")
+    return db.query("DROP TABLE IF EXISTS articles CASCADE")
   })
   .then(()=>{
-    return db.query("DROP TABLE IF EXISTS users")
+    return db.query("DROP TABLE IF EXISTS users CASCADE")
   })
   .then(()=>{
-    return db.query("DROP TABLE IF EXISTS topics")
+    return db.query("DROP TABLE IF EXISTS topics CASCADE")
   })
   .then(()=>{
-    createTopicData(topicData)
+    return createTopicData(topicData)
   })
   .then(()=>{
     return createUserData(userData)
@@ -26,22 +26,6 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
   })
 }
 
-function createTopicData(topics) {
-  return db.query(`CREATE TABLE topics (
-    slug VARCHAR PRIMARY KEY,
-    description VARCHAR NOT NULL,
-    img_url VARCHAR(1000) NOT NULL)`).then(()=>{
-      const insertedData = topics.map((topic) => [
-        topic.slug,
-        topic.description,
-        topic.img_url,
-      ]);
-      const insertedStr = format(
-        `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *`,insertedData);
-        
-        return db.query(insertedStr);
-    })
-}
 function createUserData(users) {
   return db.query(`CREATE TABLE users (
     username VARCHAR PRIMARY KEY,
@@ -58,16 +42,33 @@ function createUserData(users) {
         return db.query(insertedStr)
     })
 }
+function createTopicData(topics) {
+  return db.query(`CREATE TABLE topics (
+    slug VARCHAR PRIMARY KEY,
+    description VARCHAR NOT NULL,
+    img_url VARCHAR(1000) NOT NULL)`).then(()=>{
+      const insertedData = topics.map((topic) => [
+        topic.slug,
+        topic.description,
+        topic.img_url,
+      ]);
+      const insertedStr = format(
+        `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING *`,insertedData);
+        
+        return db.query(insertedStr);
+    })
+}   
 function createArticleData(articles) {
   return db.query(`CREATE TABLE articles (
-    article_id SERIAL PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    topic VARCHAR REFERENCES topics(slug),
-    author VARCHAR REFERENCES users(username),
-    body TEXT,
-    created_at TIMESTAMP,
-    votes INT DEFAULT 0,
-    article_img_url VARCHAR(1000) NOT NULL)`).then(()=>{
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR NOT NULL,
+      topic VARCHAR NOT NULL REFERENCES topics(slug) ON DELETE CASCADE,
+      author VARCHAR NOT NULL REFERENCES users(username) ON DELETE CASCADE,
+      body TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      votes INT DEFAULT 0,
+      article_img_url VARCHAR(1000) NOT NULL
+    )`).then(()=>{
       const insertedData = articles.map((article) => [
         article.title,
         article.topic,
@@ -87,12 +88,12 @@ function createArticleData(articles) {
 }
 function createCommentData(comments) {
   return db.query(`CREATE TABLE comments (
-    comment_id SERIAL PRIMARY KEY,
-    article_id INT REFERENCES articles(article_id),
-    body TEXT,
-    votes INT DEFAULT 0,
-    author VARCHAR REFERENCES users(username),
-    created_at TIMESTAMP)`).then(()=>{
+      comment_id SERIAL PRIMARY KEY,
+      article_id INT REFERENCES articles(article_id) ON DELETE CASCADE,
+      author VARCHAR NOT NULL REFERENCES users(username) ON DELETE CASCADE,
+      body TEXT NOT NULL,
+      votes INT DEFAULT 0,
+      created_at TIMESTAMP DEFAULT NOW())`).then(()=>{
       const insertedData = comments.map((comment) => [
         comment.article_id,
         comment.body,
