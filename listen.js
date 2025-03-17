8e5a737ddf53840062d39c99d2a5b27672df0e41
Katie Hardy <katiehardy@MacBookPro-2.broadband>
2025-03-17 21:42:23 +0000

    changes to listen file

--- listen.js
+++ listen.js
@@ -1,7 +1,6 @@
 const app = require("./app"); // Import the app from app.js
 const express = require("express")
 const { PORT = 9090 } = process.env
-const app = express()
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}...`);
