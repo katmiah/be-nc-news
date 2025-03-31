859fdd492c358c43c368f710309d5a9e2fed7ff1
Katie Hardy <katiehardy@macbookpro.home>
2025-03-31 10:15:11 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,54 +1,53 @@
-const express = require("express")
-const app = express()
-const { handlePsqlErrors } = require("./controller/controller.js")
-const { getEndpoints,
-        getTopics,
-        getArticleById,
-        getArticles, 
-        getCommentsById,
-        postCommentByArticleId,
-        patchCommentVotes,
-        deleteComment,
-        getUsers } = require("./controller/controller.js")
-        
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { handlePsqlErrors } = require("./controller/controller.js");
+const {
+  getEndpoints,
+  getTopics,
+  getArticleById,
+  getArticles,
+  getCommentsById,
+  postCommentByArticleId,
+  patchCommentVotes,
+  deleteComment,
+  getUsers,
+} = require("./controller/controller.js");
 
-app.use(express.json())
-         
-app.get("/api", getEndpoints)
+app.use(cors());
 
-app.get("/api/topics", getTopics)
+app.use(express.json());
 
-app.get("/api/articles/:article_id", getArticleById)
+app.get("/api", getEndpoints);
 
-app.get("/api/articles", getArticles)
+app.get("/api/topics", getTopics);
 
-app.get("/api/articles/:article_id/comments", getCommentsById)
+app.get("/api/articles/:article_id", getArticleById);
 
-app.post("/api/articles/:article_id/comments", postCommentByArticleId)
+app.get("/api/articles", getArticles);
 
-app.patch("/api/articles/:article_id", patchCommentVotes)
+app.get("/api/articles/:article_id/comments", getCommentsById);
 
-app.delete("/api/comments/:comment_id", deleteComment)
+app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 
-app.get("/api/users", getUsers)
+app.patch("/api/articles/:article_id", patchCommentVotes);
 
-app.use(handlePsqlErrors)
+app.delete("/api/comments/:comment_id", deleteComment);
+
+app.get("/api/users", getUsers);
+
+app.use(handlePsqlErrors);
 
 app.use((error, request, response, next) => {
-    if(error.status && error.message) {
-        response.status(error.status)
-        .send({ message: error.message })
-    } else {
-        response.status(500).send({ message: "Internal Server Error" })
-    }
-})
+  if (error.status && error.message) {
+    response.status(error.status).send({ message: error.message });
+  } else {
+    response.status(500).send({ message: "Internal Server Error" });
+  }
+});
 
 app.all("*", (request, response, next) => {
-    response.status(404)
-    .send({ message: "Path not found."})
-})
-
-
-
+  response.status(404).send({ message: "Path not found." });
+});
 
 module.exports = app;
